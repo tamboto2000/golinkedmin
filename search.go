229f1bc824aec1966525212aa3d193baa5ec008e
Franklin Collin Tamboto <tamboto2000@gmail.com>
2020-11-22 20:49:58 +0700

    Ignore go.sum

--- search.go
+++ search.go
@@ -1,13 +1,24 @@
 package golinkedmin
 
+import "github.com/tamboto2000/golinkedin"
+
+// Search types
+const (
+	SearchProfile = "PROFILE"
+	SearchCompany = "COMPANY"
+	SearchSchool  = "SCHOOL"
+	SearchGroup   = "GROUP"
+	SearchSkill   = "SKILL"
+)
+
 // Search contains search result
 type Search struct {
 	Profiles  []Profile `json:"profiles,omitempty"`
 	Companies []Company `json:"companies,omitempty"`
 	Schools   []School  `json:"schools,omitempty"`
 	Groups    []Group   `json:"groups,omitempty"`
-	Paging    Paging    `json:"paging,omitempty"`
 	Skills    []Skill   `json:"skills,omitmepty"`
+	Paging    Paging    `json:"paging,omitempty"`
 
 	ln *Linkedin
 }
@@ -23,4 +34,9 @@ type Paging struct {
 // SetLinkedin set Linkedin client
 func (srch *Search) SetLinkedin(ln *Linkedin) {
 	srch.ln = ln
+}
+
+// SearchPeople search people by keywords and filter
+func (ln *Linkedin) SearchPeople(keywords string, filter *golinkedin.PeopleSearchFilter) (*Search, error) {
+	return nil, nil
 }
